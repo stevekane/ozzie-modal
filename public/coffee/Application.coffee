9f6e618bf352878395325349c8c91677b7454429
steve kane <skane@4-south.com>
2013-08-23 14:20:53 -0500

    base project as modal exapmle for ozzie

--- public/coffee/Application.coffee
+++ public/coffee/Application.coffee
@@ -1,3 +1,34 @@
-window.Stylecore = Ember.Application.create()
+$listOfShit = $ '#listOfShit'
+$myModal = $ '#myModal'
+input1 = $myModal.find('#input1')
+input2 = $myModal.find('#input2')
+input3 = $myModal.find('#input3')
 
-require "Router.js"
+$myModalSaveButton = $('#myModal').find '#myModalSave'
+$myModalButton = $ '#myModalButton'
+
+resetForm = ->
+  input1.val("")
+  input2.val("")
+  input3.val("")
+  
+
+saveData = ->
+  val1 = input1.val()
+  val2 = input2.val()
+  val3 = input3.val()
+  data = {val1: val1, val2: val2, val3: val3}
+
+  $listOfShit.append(val1 + val2 + val3)
+  $.ajax('some/fake/endpoint', {
+    method: "POST"
+    data: data
+  })
+
+  resetForm()
+
+$myModalButton.on 'click', -> $myModal.modal 'toggle'
+$myModalSaveButton.on 'click', saveData
+
+$myModal.modal
+  show: false
--- public/dist/app.js
+++ public/dist/app.js
@@ -1,6 +1,53 @@
 minispade.register('Application.js', function() {
-window.Stylecore = Ember.Application.create();
-minispade.require("Router.js");
+var $listOfShit, $myModal, $myModalButton, $myModalSaveButton, input1, input2, input3, resetForm, saveData;
+
+$listOfShit = $('#listOfShit');
+
+$myModal = $('#myModal');
+
+input1 = $myModal.find('#input1');
+
+input2 = $myModal.find('#input2');
+
+input3 = $myModal.find('#input3');
+
+$myModalSaveButton = $('#myModal').find('#myModalSave');
+
+$myModalButton = $('#myModalButton');
+
+resetForm = function() {
+  input1.val("");
+  input2.val("");
+  return input3.val("");
+};
+
+saveData = function() {
+  var data, val1, val2, val3;
+  val1 = input1.val();
+  val2 = input2.val();
+  val3 = input3.val();
+  data = {
+    val1: val1,
+    val2: val2,
+    val3: val3
+  };
+  $listOfShit.append(val1 + val2 + val3);
+  $.ajax('some/fake/endpoint', {
+    method: "POST",
+    data: data
+  });
+  return resetForm();
+};
+
+$myModalButton.on('click', function() {
+  return $myModal.modal('toggle');
+});
+
+$myModalSaveButton.on('click', saveData);
+
+$myModal.modal({
+  show: false
+});
 
 });
 
